b848648ab6200db106fd3c02f3ab32cca5154df2
soneda-yusuke <soneda-yusuke@KYCNT2003-05.local>
2020-09-22 12:44:06 +0900

    とりあえず動くようになった

---
+++ src/js/app.js
@@ -0,0 +1,38 @@
+let pupilElements = document.getElementsByClassName("inochi__pupil");
+
+const init = (elements) => {
+  const dataArray = [];
+  Array.from(elements, (element) => {
+    const clientRect = element.getBoundingClientRect();
+    dataArray.push({
+      element,
+      x: clientRect.x,
+      y: clientRect.y,
+      width: clientRect.width,
+      height: clientRect.height,
+    });
+  });
+  return dataArray;
+};
+
+const calcPos = (mouse, elePos, eleWidth) => {
+  if (mouse - elePos > eleWidth * 1.5) {
+    return eleWidth;
+  } else if (mouse - elePos <= 0.5 * eleWidth) {
+    return 0;
+  } else {
+    return mouse - elePos - eleWidth * 0.5;
+  }
+};
+
+window.addEventListener("load", () => {
+  const pupilInfos = init(pupilElements);
+
+  window.addEventListener("mousemove", (e) => {
+    Array.from(pupilInfos, (pupilData) => {
+      const posX = calcPos(e.clientX, pupilData.x, pupilData.width);
+      const posY = calcPos(e.clientY, pupilData.y, pupilData.height);
+      pupilData.element.setAttribute("style", `left: ${posX}px; top: ${posY}px`);
+    });
+  });
+});
